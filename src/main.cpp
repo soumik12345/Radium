500fd230b90fe83926b9ed248f72db5440b3fb71
soumik12345 <19soumik.rakshit96@gmail.com>
2021-04-04 08:40:14 +0530

    Added Demo 2

--- src/main.cpp
+++ src/main.cpp
@@ -8,31 +8,43 @@ int main() {
     renderer.setCameraPosition(50, 50, 295.6);
     renderer.setCameraDirection(0, -0.042612, -1);
 
-//    renderer.importCamera("../scenes/cameras/cornell_box_1_camera.csv");
-//    renderer.importWorld("../scenes/worlds/cornell_box_1_world.csv");
+    renderer.importCamera("../scenes/cameras/demo_2_camera.csv");
+    renderer.importWorld("../scenes/worlds/demo_2_world.csv");
 
-    renderer.addObject(
-            Sphere(1e5, Vector3(1e5 + 1, 40.8, 81.6), Vector3(), Vector3(.75, .25, .25), DIFFUSE));
-    renderer.addObject(
-            Sphere(1e5, Vector3(-1e5 + 99, 40.8, 81.6), Vector3(), Vector3(.25, .25, .75), DIFFUSE));
-    renderer.addObject(
-            Sphere(1e5, Vector3(50, 40.8, 1e5), Vector3(), Vector3(.75, .75, .75), DIFFUSE));
-    renderer.addObject(
-            Sphere(1e5, Vector3(50, 40.8, -1e5 + 170), Vector3(), Vector3(), DIFFUSE));
-    renderer.addObject(
-            Sphere(1e5, Vector3(50, 1e5, 81.6), Vector3(), Vector3(.75, .75, .75), DIFFUSE));
-    renderer.addObject(
-            Sphere(1e5, Vector3(50, -1e5 + 81.6, 81.6), Vector3(), Vector3(.75, .75, .75), DIFFUSE));
-    renderer.addObject(
-            Sphere(16.5, Vector3(27, 16.5, 47), Vector3(), Vector3(1, 1, 1) * .999, SPECULAR));
-    renderer.addObject(
-            Sphere(16.5, Vector3(73, 16.5, 78), Vector3(), Vector3(1, 1, 1) * .999, REFRACTED));
-    renderer.addObject(
-            Sphere(600, Vector3(50, 681.6 - .27, 81.6), Vector3(12, 12, 12), Vector3(), DIFFUSE));
+//    Vector3 center(50, -20, -860);
+//
+//    renderer.addObject(
+//            Sphere(
+//                    160,  center + Vector3(0, 600, -500),
+//                    Vector3(1, 1, 1) * 2e2, Vector3(), DIFFUSE));
+//    renderer.addObject(
+//            Sphere(
+//                    800, center + Vector3(0, -880, -9120),
+//                    Vector3(1, 1, 1) * 2e1, Vector3(), DIFFUSE));
+//    renderer.addObject(
+//            Sphere(
+//                    10000, center + Vector3(0, 0, -200),
+//                    Vector3(0.0627, 0.188, 0.569) * 1e0, Vector3(1, 1, 1) * .4, DIFFUSE));
+//    renderer.addObject(
+//            Sphere(
+//                    800, center + Vector3(0, -720, -200),
+//                    Vector3(), Vector3(0.110, 0.898, 1.00) * .996, REFRACTED));
+//    renderer.addObject(
+//            Sphere(
+//                    790, center + Vector3(0, -720, -200),
+//                    Vector3(), Vector3(.4, .3, .04) * .6, DIFFUSE));
+//    renderer.addObject(
+//            Sphere(
+//                    325, center + Vector3(0, -255, -50),
+//                    Vector3(), Vector3(.4, .3, .04) * .8, DIFFUSE));
+//    renderer.addObject(
+//            Sphere(
+//                    275, center + Vector3(0, -205, -33),
+//                    Vector3(), Vector3(.02, .3, .02) * .75, DIFFUSE));
 
     renderer.render(false);
-    renderer.exportWorld("cornell_box_1_world.csv");
-    renderer.exportCamera("cornell_box_1_camera.csv");
+//    renderer.exportWorld("demo_2_world.csv");
+//    renderer.exportCamera("demo_2_camera.csv");
 
     return 0;
 }
