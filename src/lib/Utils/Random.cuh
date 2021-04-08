0d344482577d77bee9fbf3f202ae616dd5e888fe
soumik12345 <19soumik.rakshit96@gmail.com>
2021-04-08 11:56:44 +0530

    Added Random number generator

---
+++ src/lib/Utils/Random.cuh
@@ -0,0 +1,20 @@
+#ifndef RADIUM_UTIL_RANDOM_H
+#define RADIUM_UTIL_RANDOM_H
+
+
+__device__ static float getrandom(unsigned int *seed0, unsigned int *seed1) {
+    *seed0 = 36969 * ((*seed0) & 65535) + ((*seed0) >> 16);
+    *seed1 = 18000 * ((*seed1) & 65535) + ((*seed1) >> 16);
+
+    unsigned int ires = ((*seed0) << 16) + (*seed1);
+
+    // Convert to float
+    union {
+        float f;
+        unsigned int ui;
+    } res;
+
+    res.ui = (ires & 0x007fffff) | 0x40000000;
+
+    return (res.f - 2.f) / 2.f;
+}
